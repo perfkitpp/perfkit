2373d295886316f616c95075f87534722432993c
kangsw <ki6080@gmail.com>
2021-11-20 17:22:27 +0900

    Implement base64

--- extensions/net-provider/src/net-provider.cpp
+++ extensions/net-provider/src/net-provider.cpp
@@ -1,6 +1,6 @@
 #include "perfkit/extension/net-provider.hpp"
 
-#include "net_terminal.hpp"
+#include "net-terminal.hpp"
 
 perfkit::terminal_ptr perfkit::terminal::net::create(const struct terminal_init_info& info) {
   return std::make_shared<net::terminal>(info);
---
+++ extensions/net-provider/src/net-terminal.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <perfkit/common/assert.hxx>
+#include <perfkit/terminal.h>
+
+namespace perfkit::terminal::net {
+class terminal : public if_terminal {
+  using init_info = terminal_init_info;
+
+ public:
+  explicit terminal(init_info const&) {
+  }
+
+  commands::registry* commands() override {
+    return nullptr;
+  }
+  std::optional<std::string> fetch_command(milliseconds timeout) override {
+    return std::optional<std::string>();
+  }
+  void push_command(std::string_view command) override {
+  }
+  void write(std::string_view str, termcolor fg, termcolor bg) override {
+  }
+  std::shared_ptr<spdlog::sinks::sink> sink() override {
+    return std::shared_ptr<spdlog::sinks::sink>();
+  }
+
+};
+
+}  // namespace perfkit::terminal::net
