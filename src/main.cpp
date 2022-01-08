30964b88d1e08579daed440c29670710b9b3a6f6
ki6080 <ki6080@gmail.com>
2022-01-08 22:10:53 +0900

    Add file for gather and compile sources

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,11 @@
 //
 // Created by Seungwoo on 2021-08-25.
 //
+
 #include "perfkit/terminal.h"
 
 #define CPPHEADERS_IMPLEMENT_ASSERTIONS
 #include <perfkit/common/assert.hxx>
+
+//
+#include <perfkit/common/__cpph__.inc>
