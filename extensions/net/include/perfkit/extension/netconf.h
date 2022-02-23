20dc42276d66a99c8655534fdebb3e6ee8a4fdc1
swkang <swkang@pxscope.com>
2022-02-23 10:41:42 +0900

    Remove redundant readonly flag from netconfig

--- extensions/net/include/perfkit/extension/netconf.h
+++ extensions/net/include/perfkit/extension/netconf.h
@@ -38,7 +38,6 @@ PERFKIT_T_CATEGORY(
 
         PERFKIT_T_CONFIGURE(session_name, "default")
                 .env("PERFKIT_NET_SESSION_NAME")
-                .readonly()
                 .confirm();
 
         PERFKIT_T_CONFIGURE(session_description, "")
@@ -46,23 +45,19 @@ PERFKIT_T_CATEGORY(
 
         PERFKIT_T_CONFIGURE(auth, "")
                 .description("ID:PW:ACCESS;ID:PW:ACCESS;...")
-                .readonly()
                 .env("PERFKIT_NET_AUTH")
                 .confirm();
 
         PERFKIT_T_CONFIGURE(has_relay_server, false)
                 .env("PERFKIT_NET_USE_RELAY_SERVER")
-                .readonly()
                 .confirm();
 
         PERFKIT_T_CONFIGURE(ipaddr, "0.0.0.0")
                 .env("PERFKIT_NET_IPADDR")
-                .readonly()
                 .confirm();
 
         PERFKIT_T_CONFIGURE(port, 15572)
                 .env("PERFKIT_NET_PORT")
-                .readonly()
                 .confirm();
 
 );
