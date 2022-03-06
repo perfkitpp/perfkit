07e4a49e305b386f03c99886162344fd6a201f2b
ki6080 <ki6080@gmail.com>
2022-03-06 18:03:19 +0900

    net-v2: Add basic files for new version

---
+++ extensions/net-v2/include/perfkit/extension/net.hpp
@@ -0,0 +1,66 @@
+// MIT License
+//
+// Copyright (c) 2021-2022. Seungwoo Kang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// project home: https://github.com/perfkitpp
+
+#pragma once
+
+#include "net.hpp"
+#include "perfkit/configs.h"
+#include "perfkit/terminal.h"
+
+namespace perfkit::terminal::net {
+
+struct profile;
+
+perfkit::terminal_ptr create(profile const& cfg);
+
+PERFKIT_T_CATEGORY(
+        profile,
+
+        PERFKIT_T_CONFIGURE(session_name, "default")
+                .env("PERFKIT_NET_SESSION_NAME")
+                .confirm();
+
+        PERFKIT_T_CONFIGURE(session_description, "")
+                .confirm();
+
+        PERFKIT_T_CONFIGURE(auth, "")
+                .description("ID:PW:ACCESS;ID:PW:ACCESS;...")
+                .env("PERFKIT_NET_AUTH")
+                .confirm();
+
+        PERFKIT_T_CONFIGURE(has_relay_server, false)
+                .env("PERFKIT_NET_USE_RELAY_SERVER")
+                .confirm();
+
+        PERFKIT_T_CONFIGURE(ipaddr, "0.0.0.0")
+                .env("PERFKIT_NET_IPADDR")
+                .confirm();
+
+        PERFKIT_T_CONFIGURE(port, 15572)
+                .env("PERFKIT_NET_PORT")
+                .confirm();
+
+);
+
+}  // namespace perfkit::terminal::net
---
+++ extensions/net-v2/src/utils.hpp
@@ -0,0 +1,56 @@
+// MIT License
+//
+// Copyright (c) 2021-2022. Seungwoo Kang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// project home: https://github.com/perfkitpp
+
+#pragma once
+#include <functional>
+#include <memory>
+#include <string>
+
+#include <spdlog/fwd.h>
+
+#include "perfkit/common/utility/inserter.hxx"
+
+namespace perfkit::terminal::net::detail {
+std::string try_fetch_input(int ms_to_wait);
+std::shared_ptr<spdlog::logger> nglog();
+
+struct proc_stat_t
+{
+    double cpu_usage_total_user;
+    double cpu_usage_total_system;
+    double cpu_usage_self_user;
+    double cpu_usage_self_system;
+
+    int64_t memory_usage_virtual;
+    int64_t memory_usage_resident;
+    int32_t num_threads;
+};
+
+bool fetch_proc_stat(proc_stat_t* ostat);
+
+void input_redirect(std::function<void(char)> inserter);
+void input_rollback();
+
+void write(char const* buffer, size_t n);
+}  // namespace perfkit::terminal::net::detail
