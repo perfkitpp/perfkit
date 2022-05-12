5e653f019b5550a77ad59e86f3b00b48a1a84c37
swkang <swkang@pxscope.com>
2022-05-12 17:51:56 +0900

    net-v2: Add net termnial template

---
+++ modules/net-v2/apptemplate/perfkit-appbase.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+
+#include <perfkit/terminal.h>
+
+int main(int argc, char** argv);
+
+namespace perfkit {
+class AppBase
+{
+    friend int ::main(int, char**);
+
+   public:
+    virtual ~AppBase() noexcept = default;
+
+   public:
+    virtual std::string DefaultConfigPath() const noexcept { return ""; }
+    virtual void S00_Opt_InitWithCmdLineArgs(array_view<char const*> args) {}
+    virtual void S01_PreLoadConfigs() {}
+    virtual void S02_PostLoadConfigs() {}
+    virtual void S03_LaunchApplication() {}
+    virtual void S04_ConfigureTerminalCommands(if_terminal*) {}
+    virtual void S05_SetInitialCommands(if_terminal*) {}
+    virtual void Tick_Application(float delta_time) {}
+    virtual void P01_DisposeApplication() {}
+};
+}  // namespace perfkit
+
+#define PERFKIT_USE_APP_CLASS(YourAppClass)                         \
+    std::unique_ptr<perfkit::AppBase> INTERNAL_perfkit_create_app() \
+    {                                                               \
+        return std::make_unique<YourAppClass>();                    \
+    }
---
+++ modules/net-v2/apptemplate/perfkit-main.cpp
@@ -0,0 +1,112 @@
+#include <csetjmp>
+#include <csignal>
+
+#include "cpph/timer.hxx"
+#include "perfkit-appbase.hpp"
+#include "perfkit/configs.h"
+#include "perfkit/extension/net.hpp"
+#include "spdlog/spdlog.h"
+
+// Turn off
+static std::atomic_bool g_server_is_alive = true;
+
+// Handle sigint
+static void sigint_handler(int signum)
+{
+    static int times = 5;
+    spdlog::info("SIGINT received. Force exit: {} / {}", --times / 5);
+    g_server_is_alive = false;
+
+    if (times == 0) {
+        spdlog::warn("Next SIGINT will terminate this process.");
+        signal(SIGINT, nullptr);
+    } else {
+        signal(SIGINT, &sigint_handler);
+    }
+}
+
+std::unique_ptr<perfkit::AppBase> INTERNAL_perfkit_create_app();
+
+int main(int argc, char** argv)
+{
+    using namespace cpph;
+
+    spdlog::info("{}", argv[0]);
+    spdlog::info("Creating application instance ...");
+    auto app = INTERNAL_perfkit_create_app();
+
+    try {
+        spdlog::info("Parsing {} command line arguments", argc);
+        perfkit::configs::parse_args(&argc, &argv, true);
+    } catch (std::exception& e) {
+        spdlog::error("Error during flag parsing: \n{}", e.what());
+        return 1;
+    }
+
+    spdlog::info("APP INIT 0 - PreLoadConfigs");
+    app->S01_PreLoadConfigs();
+
+    spdlog::info("Loading configs ...");
+    std::string confpath = argc == 1 ? app->DefaultConfigPath() : argv[1];
+    {
+        if (perfkit::configs::import_file(confpath)) {
+            spdlog::info("Successfully loaded intial config '{}'", confpath);
+        } else if (confpath.empty()) {
+            spdlog::critical("Config path is not specified!");
+            return 1;
+        } else {
+            spdlog::warn("Failed to load config '{}'", confpath);
+        }
+    }
+
+    if (argc > 2) {
+        spdlog::info("APP INIT 0 - Processing {} positional commandline args ...", argc - 2);
+        app->S00_Opt_InitWithCmdLineArgs({(char const**)argv, size_t(argc - 2)});
+    }
+
+    spdlog::info("APP INIT 1 - PostLoadConfigs");
+    app->S02_PostLoadConfigs();
+
+    auto term = perfkit::terminal::net::create();
+    perfkit::terminal::register_conffile_io_commands(term.get());
+    term->launch_stdin_fetch_thread();
+
+    // Install signal handler
+    signal(SIGINT, &sigint_handler);
+
+    // Initialize application
+    spdlog::info("Launching application ...");
+    app->S03_LaunchApplication();
+
+    // Configure commands
+    spdlog::info("Adding application configs ...");
+    term->add_command("quit", [] { g_server_is_alive = false; });
+    app->S04_ConfigureTerminalCommands(term.get());
+
+    // Queue save command
+    term->push_command("save-config " + std::string(confpath));
+    app->S05_SetInitialCommands(term.get());
+
+    // Command receive loop
+    stopwatch sw;
+
+    while (g_server_is_alive) {
+        app->Tick_Application(sw.elapsed().count()), sw.reset();
+        auto cmd = term->fetch_command(200ms);
+        if (not cmd) { continue; }
+
+        spdlog::info("CMD: {}", *cmd);
+        term->invoke_command(move(*cmd));
+    }
+
+    spdlog::info("Disposing application");
+    app->P01_DisposeApplication();
+
+    spdlog::info("Destroying application instance");
+    app.reset();
+
+    spdlog::info("Disposing terminal.");
+    term.reset();
+
+    return 0;
+}
