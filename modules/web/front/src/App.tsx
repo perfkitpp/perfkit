cb567fe2566cda533d672475872a4c7a157a48e7
ki6080 <ki6080@gmail.com>
2022-09-25 09:26:33 +0900

    web.front: Create APIs for creating/destroying windows

--- modules/web/front/src/App.tsx
+++ modules/web/front/src/App.tsx
@@ -1,10 +1,10 @@
-import { createContext, CSSProperties, useEffect, useMemo, useRef, useState } from 'react';
+import {createContext, CSSProperties, ReactNode, useEffect, useMemo, useRef, useState} from 'react';
 import './App.scss';
 import Terminal from "./comp/Terminal";
-import { Button, Container, Row, Col, ColProps } from "react-bootstrap";
+import {Button, Container, Row, Col, ColProps, Tab} from "react-bootstrap";
 import ConfigPanel from "./comp/ConfigPanel";
 import TracePanel from './comp/TracePanel'
-import { BoxData, DockLayout, LayoutBase, LayoutData, PanelData, TabData } from "rc-dock";
+import {BoxData, DockLayout, LayoutBase, LayoutData, PanelData, TabData} from "rc-dock";
 
 export const socketUrlPrefix = process.env.NODE_ENV === "development"
   ? `ws://${window.location.hostname}:15572`
@@ -42,7 +42,56 @@ interface EnableStatus {
 
 const initStateCfgStr = localStorage.getItem('PerfkitWeb/EnableStatus');
 const initStateCfg = JSON.parse(initStateCfgStr || "{}") as EnableStatus;
+const dockLayout = {ref: null as any as DockLayout};
 
+export interface WindowInfo {
+  onClose?: () => void,
+  onOpen?: () => void,
+  content: ReactNode,
+  title?: string,
+}
+
+function WrappedContentNode(props: { info: WindowInfo }) {
+  const {info} = props;
+
+  useEffect(() => {
+    info.onOpen && info.onOpen();
+    return () => {
+      info.onClose && info.onClose();
+    };
+  }, []);
+
+  return <>{info.content}</>
+}
+
+export function CreateWindow(key: string, info: WindowInfo): boolean {
+  if (IsWindowOpen(key))
+    return false;
+
+  const data: TabData = {
+    title: info.title ?? key,
+    closable: true,
+    id: key,
+    cached: true,
+    content: <WrappedContentNode info={info}/>
+  };
+
+  dockLayout.ref.dockMove(data, 'panel-main', "float");
+  return true;
+}
+
+export function CloseWindow(key: string): boolean {
+  const data = dockLayout.ref.find(key) as null | PanelData | TabData;
+  if (data == null)
+    return false;
+
+  dockLayout.ref.dockMove(data, null, 'remove');
+  return true;
+}
+
+export function IsWindowOpen(key: string): boolean {
+  return dockLayout.ref.find(key) != null;
+}
 
 function DockWrapper(prop: {
   children?: React.ReactNode;
@@ -67,19 +116,19 @@ function App() {
                 id: 'tab-term',
                 title: 'Terminal',
                 content:
-                  <Terminal socketUrl={socketUrlPrefix + '/ws/tty'} />,
+                  <Terminal socketUrl={socketUrlPrefix + '/ws/tty'}/>,
                 cached: true
               },
               {
                 id: 'tab-cfg',
                 title: 'Configs',
-                content: <ConfigPanel socketUrl={socketUrlPrefix + '/ws/config'} />,
+                content: <ConfigPanel socketUrl={socketUrlPrefix + '/ws/config'}/>,
                 cached: true
               },
               {
                 id: 'tab-trace',
                 title: 'Traces',
-                content: <TracePanel socketUrl={socketUrlPrefix + '/ws/trace'} />,
+                content: <TracePanel socketUrl={socketUrlPrefix + '/ws/trace'}/>,
                 cached: true
               },
             ]
@@ -90,14 +139,20 @@ function App() {
   }, []);
 
   useEffect(() => {
+    dockLayout.ref = dockRef.current;
     const layoutStatus = localStorage.getItem('PerfkitWeb/LayoutStatus');
     try {
       const layout = JSON.parse(layoutStatus ?? "") as LayoutBase;
       dockRef.current.loadLayout(layout);
     } catch {
       JSON.parse(`"parse"`);
-      
     }
+
+    CreateWindow('hello, my world!', {
+      content: <div>HELLO, WORLD!</div>,
+      onOpen: () => console.log("I'm Open!"),
+      onClose: () => console.log("I'm Closed!"),
+    })
 
     window.onbeforeunload = ev => {
       const layout = dockRef.current.saveLayout();
